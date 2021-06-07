e84a80448fc523f60210c147cd7aab6943f33ad1
Björn Linse <bjorn.linse@gmail.com>
2021-06-07 10:39:10 +0200

    lalala

--- src/io.zig
+++ src/io.zig
@@ -7,24 +7,40 @@ const c = @cImport({
 fn zero(val: c_int) !void {
     if (val != 0) {
         dbg("the cow jumped over the moon: {}\n", .{val});
-        return error.CowJumpedTheMoon;
+        return error.TheCowJumpedOverTheMoon;
     }
+}
+
+fn nonneg(val: anytype) !@TypeOf(val) {
+    if (val < 0) {
+        dbg("the dinner conversation is lively: {}\n", .{val});
+        return error.TheCowJumpedOverTheMoon;
+    }
+    return val;
+}
+
+fn unconst(comptime T: type, ptr: anytype) T {
+    return @intToPtr(T, @ptrToInt(ptr));
+}
+
+fn uv_buf(val: []const u8) c.uv_buf_t {
+    return .{ .base = unconst([*c]u8, val.ptr), .len = val.len };
 }
 
 pub fn main() !void {
     var loop: c.uv_loop_t = undefined;
+    try zero(c.uv_loop_init(&loop));
 
-    _ = c.uv_loop_init(&loop);
+    var stdin_pipe: c.uv_pipe_t = undefined;
+    try zero(c.uv_pipe_init(&loop, &stdin_pipe, 0));
+    var stdin = @ptrCast([*c]c.uv_stream_t, &stdin_pipe);
 
-    var proc: c.uv_process_t = undefined;
-
-    var stdin: c.uv_pipe_t = undefined;
-    try zero(c.uv_pipe_init(&loop, &stdin, 0));
     var stdout: c.uv_pipe_t = undefined;
     try zero(c.uv_pipe_init(&loop, &stdout, 0));
+
     const Flags = c.uv_stdio_flags;
 
-    var uvstdio: [3]c.uv_stdio_container_t = .{ .{ .flags = @intToEnum(Flags, c.UV_CREATE_PIPE | c.UV_READABLE_PIPE), .data = .{ .stream = @ptrCast([*c]c.uv_stream_t, &stdin) } }, .{ .flags = @intToEnum(Flags, c.UV_CREATE_PIPE | c.UV_WRITABLE_PIPE), .data = .{ .stream = @ptrCast([*c]c.uv_stream_t, &stdout) } }, .{ .flags = @intToEnum(Flags, c.UV_INHERIT_FD), .data = .{ .fd = 2 } } };
+    var uvstdio: [3]c.uv_stdio_container_t = .{ .{ .flags = @intToEnum(Flags, c.UV_CREATE_PIPE | c.UV_READABLE_PIPE), .data = .{ .stream = stdin } }, .{ .flags = @intToEnum(Flags, c.UV_CREATE_PIPE | c.UV_WRITABLE_PIPE), .data = .{ .stream = @ptrCast([*c]c.uv_stream_t, &stdout) } }, .{ .flags = @intToEnum(Flags, c.UV_INHERIT_FD), .data = .{ .fd = 2 } } };
 
     const args = &[_:null]?[*:0]u8{ "nvim", "--embed" };
     const yarg = @intToPtr([*c][*c]u8, @ptrToInt(args)); // yarrrg
@@ -42,8 +58,14 @@ pub fn main() !void {
         .exit_cb = null,
     };
 
+    var proc: c.uv_process_t = undefined;
     var status = c.uv_spawn(&loop, &proc, &proc_opt);
     dbg("{}\n", .{status});
+
+    var buf = uv_buf("lalala");
+
+    var len = try nonneg(c.uv_try_write(stdin, &buf, 1));
+    dbg("lenny {}\n", .{len});
 
     if (c.uv_loop_close(&loop) != 0) {
         dbg("very error\n", .{});
