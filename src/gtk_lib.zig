236aa04d0a5fcd866eac3b8897d44c935b8b0d1a
Björn Linse <bjorn.linse@gmail.com>
2021-06-26 17:57:48 +0200

    JAJA SEHR RENSIG

--- src/gtk_lib.zig
+++ src/gtk_lib.zig
@@ -2,18 +2,30 @@ const std = @import("std");
 const c = @import("gtk_c.zig");
 
 // translate-C of GTK_WINDOW etc macros fails, let's doit ourselves
-fn gtk_cast(comptime T: type, gtk_type: anytype, value: anytype) *T {
+pub fn g_cast(comptime T: type, gtk_type: anytype, value: anytype) *T {
     return @ptrCast(*T, @alignCast(std.meta.alignment(T), c.g_type_check_instance_cast(@ptrCast([*c]c.GTypeInstance, @alignCast(std.meta.alignment(c.GTypeInstance), value)), gtk_type)));
 }
 
 pub fn GTK_WINDOW(value: anytype) *c.GtkWindow {
-    return gtk_cast(c.GtkWindow, c.gtk_window_get_type(), value);
+    return g_cast(c.GtkWindow, c.gtk_window_get_type(), value);
 }
 
 pub fn GTK_BOX(value: anytype) *c.GtkBox {
-    return gtk_cast(c.GtkBox, c.gtk_box_get_type(), value);
+    return g_cast(c.GtkBox, c.gtk_box_get_type(), value);
 }
 
 pub fn G_CALLBACK(value: anytype) c.GCallback {
     return @ptrCast(c.GCallback, @alignCast(std.meta.alignment(fn () callconv(.C) void), value));
+}
+
+pub fn g_signal_connect_data(instance: anytype, detailed_signal: [*:0]const u8, handler: anytype, data: anytype, flags: anytype) c.gulong {
+    return c.g_signal_connect_data(instance, detailed_signal, handler, data, null, std.zig.c_translation.cast(c.GConnectFlags, flags));
+}
+
+pub fn g_signal_connect(instance: anytype, detailed_signal: [*:0]const u8, handler: anytype, data: anytype) c.gulong {
+    return g_signal_connect_data(instance, detailed_signal, handler, data, 0);
+}
+
+pub fn g_signal_connect_swapped(instance: anytype, detailed_signal: [*:0]const u8, handler: anytype, data: anytype) c.gulong {
+    return g_signal_connect_data(instance, detailed_signal, handler, data, c.G_CONNECT_SWAPPED);
 }
--- src/gtk_main.zig
+++ src/gtk_main.zig
@@ -3,8 +3,6 @@
 const std = @import("std");
 const c = @import("gtk_c.zig");
 const g = @import("gtk_lib.zig");
-const GTK_WINDOW = g.GTK_WINDOW;
-const G_CALLBACK = g.G_CALLBACK;
 
 pub fn print_hello(arg_widget: [*c]c.GtkWidget, arg_data: c.gpointer) callconv(.C) void {
     var widget = arg_widget;
@@ -18,12 +16,12 @@ pub fn activate(app: [*c]c.GtkApplication, user_data: c.gpointer) callconv(.C) v
     _ = user_data;
     var window: *c.GtkWidget = c.gtk_application_window_new(app);
     c.gtk_window_set_title(g.GTK_WINDOW(window), "Window");
-    c.gtk_window_set_default_size(g.GTK_WINDOW(window), @as(c_int, 200), @as(c_int, 200));
+    c.gtk_window_set_default_size(g.GTK_WINDOW(window), 200, 200);
     var box: *c.GtkWidget = c.gtk_box_new(@bitCast(c_uint, c.GTK_ORIENTATION_HORIZONTAL), @as(c_int, 0));
     c.gtk_window_set_child(g.GTK_WINDOW(window), box);
     var button: *c.GtkWidget = c.gtk_button_new_with_label("Hello World");
-    _ = c.g_signal_connect_data(@ptrCast(c.gpointer, button), "clicked", G_CALLBACK(print_hello), @intToPtr(?*c_void, @as(c_int, 0)), null, @bitCast(c_uint, @as(c_int, 0)));
-    _ = c.g_signal_connect_data(@ptrCast(c.gpointer, button), "clicked", G_CALLBACK(c.gtk_window_destroy), @ptrCast(c.gpointer, window), null, @bitCast(c_uint, c.G_CONNECT_SWAPPED));
+    _ = g.g_signal_connect(button, "clicked", g.G_CALLBACK(print_hello), null);
+    _ = g.g_signal_connect_swapped(button, "clicked", g.G_CALLBACK(c.gtk_window_destroy), window);
     c.gtk_box_append(g.GTK_BOX(box), button);
     c.gtk_widget_show(window);
 }
@@ -31,8 +29,8 @@ pub export fn main() u8 {
     var argc = @intCast(c_int, std.os.argv.len);
     var argv = @ptrCast([*c][*c]u8, std.os.argv.ptr);
     var app: *c.GtkApplication = c.gtk_application_new("io.github.bfredl.zignvim", @bitCast(c_uint, c.G_APPLICATION_FLAGS_NONE));
-    _ = c.g_signal_connect_data(@ptrCast(c.gpointer, app), "activate", @ptrCast(c.GCallback, @alignCast(@import("std").meta.alignment(fn () callconv(.C) void), activate)), @intToPtr(?*c_void, @as(c_int, 0)), null, @bitCast(c_uint, @as(c_int, 0)));
-    var status = c.g_application_run(@ptrCast([*c]c.GApplication, @alignCast(@import("std").meta.alignment(c.GApplication), c.g_type_check_instance_cast(@ptrCast([*c]c.GTypeInstance, @alignCast(@import("std").meta.alignment(c.GTypeInstance), app)), c.g_application_get_type()))), argc, argv);
+    _ = g.g_signal_connect(app, "activate", g.G_CALLBACK(activate), c.NULL);
+    var status = c.g_application_run(g.g_cast(c.GApplication, c.g_application_get_type(), app), argc, argv);
     c.g_object_unref(@ptrCast(c.gpointer, app));
     return @intCast(u8, status);
 }
