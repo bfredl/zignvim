3c488a7cc5d5e5607ee00255cc1d42b6f9335438
Björn Linse <bjorn.linse@gmail.com>
2021-06-27 11:30:32 +0200

    super-rensich

--- src/gtk_main.zig
+++ src/gtk_main.zig
@@ -4,20 +4,18 @@ const std = @import("std");
 const c = @import("gtk_c.zig");
 const g = @import("gtk_lib.zig");
 
-pub fn print_hello(arg_widget: [*c]c.GtkWidget, arg_data: c.gpointer) callconv(.C) void {
-    var widget = arg_widget;
+pub fn print_hello(widget: *c.GtkWidget, data: c.gpointer) callconv(.C) void {
     _ = widget;
-    var data = arg_data;
     _ = data;
     c.g_print("Hello World\n");
 }
 
-pub fn activate(app: [*c]c.GtkApplication, user_data: c.gpointer) callconv(.C) void {
+pub fn activate(app: *c.GtkApplication, user_data: c.gpointer) callconv(.C) void {
     _ = user_data;
     var window: *c.GtkWidget = c.gtk_application_window_new(app);
     c.gtk_window_set_title(g.GTK_WINDOW(window), "Window");
     c.gtk_window_set_default_size(g.GTK_WINDOW(window), 200, 200);
-    var box: *c.GtkWidget = c.gtk_box_new(@bitCast(c_uint, c.GTK_ORIENTATION_HORIZONTAL), @as(c_int, 0));
+    var box: *c.GtkWidget = c.gtk_box_new(c.GTK_ORIENTATION_HORIZONTAL, 0);
     c.gtk_window_set_child(g.GTK_WINDOW(window), box);
     var button: *c.GtkWidget = c.gtk_button_new_with_label("Hello World");
     _ = g.g_signal_connect(button, "clicked", g.G_CALLBACK(print_hello), null);
@@ -28,7 +26,7 @@ pub fn activate(app: [*c]c.GtkApplication, user_data: c.gpointer) callconv(.C) v
 pub export fn main() u8 {
     var argc = @intCast(c_int, std.os.argv.len);
     var argv = @ptrCast([*c][*c]u8, std.os.argv.ptr);
-    var app: *c.GtkApplication = c.gtk_application_new("io.github.bfredl.zignvim", @bitCast(c_uint, c.G_APPLICATION_FLAGS_NONE));
+    var app: *c.GtkApplication = c.gtk_application_new("io.github.bfredl.zignvim", c.G_APPLICATION_FLAGS_NONE);
     _ = g.g_signal_connect(app, "activate", g.G_CALLBACK(activate), c.NULL);
     var status = c.g_application_run(g.g_cast(c.GApplication, c.g_application_get_type(), app), argc, argv);
     c.g_object_unref(@ptrCast(c.gpointer, app));
