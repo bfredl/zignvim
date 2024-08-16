39590269e444dcf1de2d4be4a6b3f1e340556ca9
bfredl <bjorn.linse@gmail.com>
2024-08-16 14:26:50 +0200

    very option

--- src/RPCState.zig
+++ src/RPCState.zig
@@ -8,10 +8,11 @@ const dbg = std.debug.print;
 
 const State = enum {
     next_msg,
-    notify_redraw,
+    redraw_event,
 };
 
 state: State = .next_msg,
+redraw_events: u64 = 0,
 
 pub fn init(allocator: mem.Allocator) Self {
     _ = allocator;
@@ -24,7 +25,7 @@ pub fn process(self: *Self, decoder: *mpack.Decoder) !void {
     while (true) {
         const res = try switch (self.state) {
             .next_msg => self.next_msg(decoder),
-            .notify_redraw => unreachable,
+            .redraw_event => self.redraw_event(decoder),
         };
 
         self.state = res orelse return;
@@ -33,16 +34,33 @@ pub fn process(self: *Self, decoder: *mpack.Decoder) !void {
 }
 
 fn next_msg(self: *Self, base_decoder: *mpack.Decoder) !?State {
-    _ = self;
     var decoder = base_decoder.copy();
     const tok = try decoder.expectArray() orelse return null;
-    dbg("yarr: {}\n", .{tok});
     if (tok < 3) return error.MalformatedRPCMessage;
     const num = try decoder.expectUInt() orelse return null;
-    dbg("num: {}\n", .{num});
     if (num != 2) @panic("handle replies and requests");
+    if (tok != 3) return error.MalformatedRPCMessage;
+
     const name = try decoder.expectString() orelse return null;
-    dbg("name: {s}\n", .{name});
+
+    if (!std.mem.eql(u8, name, "redraw")) @panic("handle notifications other than 'redraw'");
+
+    self.redraw_events = try decoder.expectArray() orelse return null;
+    base_decoder.accept(decoder);
+    return .redraw_event;
+}
+
+fn redraw_event(self: *Self, base_decoder: *mpack.Decoder) !?State {
+    if (self.redraw_events == 0) {
+        return .next_msg;
+    }
+
+    var decoder = base_decoder.copy();
+    const nitems = try decoder.expectArray() orelse return null;
+    if (nitems < 1) return error.MalformatedRPCMessage;
+    const name = try decoder.expectString() orelse return null;
+
+    dbg("EVENT: '{s}' with {}\n", .{ name, nitems - 1 });
 
     base_decoder.accept(decoder);
     std.posix.exit(0);
