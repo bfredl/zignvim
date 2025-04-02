c820510e75c0a3ca74baff9ec7ae18650efc233e
bfredl <bjorn.linse@gmail.com>
2025-04-02 10:21:10 +0200

    IK HAAT WERKEN IN DEN TUI

--- build.zig
+++ build.zig
@@ -1,9 +1,16 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const t = b.standardTargetOptions(.{});
     const opt = b.standardOptimizeOption(.{});
     const llvm = b.option(bool, "llvm", "use llvm") orelse true;
+    const use_vaxis = b.option(bool, "vaxis", "use vaxis") orelse false;
+    const use_gtk = b.option(bool, "gtk", "use gtk") orelse false;
+
+    if (!use_vaxis and !use_gtk) {
+        std.debug.print("use at least one of -Dvaxis or -Dgtk!", .{});
+        return error.ConfigError;
+    }
 
     const io_test = b.step("io_test", "fooka amnitel");
     const io_test_exe = b.addExecutable(.{
@@ -16,41 +23,62 @@ pub fn build(b: *std.Build) void {
     const run_cmd = b.addRunArtifact(io_test_exe);
     io_test.dependOn(&run_cmd.step);
 
-    const gtk_test = b.step("gtk_test", "visual representation");
-    const exe_gtk = b.addExecutable(.{
-        .name = "zignvim_gtk",
-        .root_source_file = b.path("src/gtk_main.zig"),
-        .optimize = opt,
-        .target = t,
-    });
-    exe_gtk.use_llvm = llvm;
-    exe_gtk.linkLibC();
-    exe_gtk.linkSystemLibrary("gtk4");
-    exe_gtk.linkSystemLibrary("ibus-1.0");
-    b.installArtifact(exe_gtk);
-    const gtk_run_cmd = b.addRunArtifact(exe_gtk);
-    if (b.args) |args| {
-        gtk_run_cmd.addArgs(args);
+    if (use_vaxis) {
+        const vaxis = b.lazyDependency("vaxis", .{ .optimize = opt, .target = t }) orelse return;
+        const tui_step = b.step("tui", "terminal representation");
+        const exe_tui = b.addExecutable(.{
+            .name = "zignvim_tui",
+            .root_source_file = b.path("src/tui_main.zig"),
+            .optimize = opt,
+            .target = t,
+        });
+        exe_tui.root_module.addImport("vaxis", vaxis.module("vaxis"));
+        exe_tui.use_llvm = llvm;
+        b.installArtifact(exe_tui);
+        const tui_run_cmd = b.addRunArtifact(exe_tui);
+        if (b.args) |args| {
+            tui_run_cmd.addArgs(args);
+        }
+        tui_step.dependOn(&tui_run_cmd.step);
     }
-    gtk_test.dependOn(&gtk_run_cmd.step);
 
-    const pango_test = b.step("pango_test", "visual representation");
-    const exe_pango = b.addExecutable(.{
-        .name = "pango_test",
-        .root_source_file = b.path("src/pango_test.zig"),
-        .optimize = opt,
-        .target = t,
-    });
-    exe_pango.use_llvm = llvm;
-    exe_pango.linkLibC();
-    exe_pango.linkSystemLibrary("gtk4");
-    exe_pango.linkSystemLibrary("ibus-1.0"); // TODO: OPTIONAL!
-    b.installArtifact(exe_pango);
-    const pango = b.addRunArtifact(exe_pango);
-    if (b.args) |args| {
-        pango.addArgs(args);
+    if (use_gtk) {
+        const gtk_test = b.step("gtk_test", "visual representation");
+        const exe_gtk = b.addExecutable(.{
+            .name = "zignvim_gtk",
+            .root_source_file = b.path("src/gtk_main.zig"),
+            .optimize = opt,
+            .target = t,
+        });
+        exe_gtk.use_llvm = llvm;
+        exe_gtk.linkLibC();
+        exe_gtk.linkSystemLibrary("gtk4");
+        exe_gtk.linkSystemLibrary("ibus-1.0");
+        b.installArtifact(exe_gtk);
+        const gtk_run_cmd = b.addRunArtifact(exe_gtk);
+        if (b.args) |args| {
+            gtk_run_cmd.addArgs(args);
+        }
+        gtk_test.dependOn(&gtk_run_cmd.step);
+
+        const pango_test = b.step("pango_test", "visual representation");
+        const exe_pango = b.addExecutable(.{
+            .name = "pango_test",
+            .root_source_file = b.path("src/pango_test.zig"),
+            .optimize = opt,
+            .target = t,
+        });
+        exe_pango.use_llvm = llvm;
+        exe_pango.linkLibC();
+        exe_pango.linkSystemLibrary("gtk4");
+        exe_pango.linkSystemLibrary("ibus-1.0"); // TODO: OPTIONAL!
+        b.installArtifact(exe_pango);
+        const pango = b.addRunArtifact(exe_pango);
+        if (b.args) |args| {
+            pango.addArgs(args);
+        }
+        pango_test.dependOn(&pango.step);
     }
-    pango_test.dependOn(&pango.step);
 
     if (false) {
         const mode = b.standardReleaseOptions();
@@ -81,9 +109,5 @@ pub fn build(b: *std.Build) void {
         const iotest_step = b.step("iotest", "Basic ");
         const run = exe.run();
         iotest_step.dependOn(&run.step);
-
-        const gtktest_step = b.step("gtktest", "Accomplished ");
-        const gtkrun = exe_gtk.run();
-        gtktest_step.dependOn(&gtkrun.step);
     }
 }
