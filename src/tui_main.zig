a963e42c126ec5b7eb1062f0d822353de7f1af19
bfredl <bjorn.linse@gmail.com>
2025-04-22 20:54:12 +0200

    disarm you with a :smile

--- src/tui_main.zig
+++ src/tui_main.zig
@@ -18,6 +18,49 @@ pub fn main() !void {
 
     const stream = xev.Stream.initFd(tty.fd);
     defer stream.deinit();
+
+    var read_buf: [1024]u8 = undefined;
+
+    var c: xev.Completion = undefined;
+    var self: void = {};
+    stream.read(&loop, &c, .{ .slice = &read_buf }, void, &self, readCb);
+
+    std.debug.print("enter\r\n", .{});
+    try loop.run(.until_done);
+    std.debug.print("exit\r\n", .{});
+}
+
+fn readCb(
+    self_: ?*void,
+    loop: *xev.Loop,
+    c: *xev.Completion,
+    stream: xev.Stream,
+    buf: xev.ReadBuffer,
+    r: xev.ReadError!usize,
+) xev.CallbackAction {
+    _ = loop;
+    _ = c;
+    _ = stream;
+    _ = self_;
+    const n = r catch |err| switch (err) {
+        error.EOF => {
+            std.debug.print("handle EOF!\n", .{});
+            return .disarm;
+        },
+        else => {
+            std.log.warn("tty unexpected err={}", .{err});
+            return .disarm;
+        },
+    };
+
+    std.debug.print("Nommm {}\r\n", .{n});
+    const slice = buf.slice[0..n];
+    if (n > 0) std.debug.print("som {}\r\n", .{slice[0]});
+    if (n > 0 and slice[0] == 3) {
+        return .disarm;
+    }
+
+    return .rearm;
 }
 
 const Event = union(enum) {
