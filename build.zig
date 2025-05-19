58f51a9a3a0bb21c6c221b7cff94ea6a28d60162
bfredl <bjorn.linse@gmail.com>
2025-05-19 09:58:53 +0200

    delet whatever shit

--- build.zig
+++ build.zig
@@ -84,28 +84,15 @@ pub fn build(b: *std.Build) !void {
 
     if (false) {
         const mode = b.standardReleaseOptions();
-        const lib = b.addStaticLibrary("zignvim", "src/main.zig");
-        lib.setBuildMode(mode);
-        //lib.install();
-
-        const exe_uv = b.addExecutable("iotest_uv", "src/io_uv.zig");
-        exe_uv.linkSystemLibrary("c");
-        exe_uv.linkSystemLibrary("uv");
-        exe_uv.setBuildMode(mode);
-        // exe_uv.install();
 
         const exe = b.addExecutable("iotest", "src/io_test.zig");
         exe.setBuildMode(mode);
         exe.install();
 
-        var main_tests = b.addTest("src/main.zig");
-        main_tests.setBuildMode(mode);
-
         var msgpack_tests = b.addTest("src/mpack.zig");
         msgpack_tests.setBuildMode(mode);
 
         const test_step = b.step("test", "Run library tests");
-        // test_step.dependOn(&main_tests.step);
         test_step.dependOn(&msgpack_tests.step);
 
         const iotest_step = b.step("iotest", "Basic ");
